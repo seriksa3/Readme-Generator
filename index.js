e76e166c7e2d942839408bc4100368502a493051
seriksa3 <kellyseriksa7@gmail.com>
2023-03-09 00:51:01 +0000

    Almost done with files

--- index.js
+++ index.js
@@ -1,61 +1,116 @@
 const fs = require("fs");
-const path = require('path');
+const path = require("path");
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
 
 const questions = [
     {
       type: 'input',
-      name: 'Title',
-      message: 'What is your name of your project?',
+      name: 'Name',
+      message: 'What is your Name?',
     },
     {
       type: 'input',
-      name: 'name',
-      message: 'Where are you from?',
+      name: 'Title',
+      message: 'What is your title of your project?',
+      validate:(value) => {
+        if (value){return true} else{return "You have to input a Title"}
+      },
     },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Give me a quck description of your Project',
+        validate:(value) => {
+          if (value){return true} else{return "You have to input a description"}
+        },
+      },
     {
       type: 'checkbox',
-      name: 'Languages used',
-      message: 'Select the languages going to be used for thos project?',
-      choices: ['Baseball','Basketball','Reading'],
+      name: 'Languages',
+      message: 'Select the languages going to be used for this project?',
+      choices: ["HTML","CSS","JavaScript","Python","Java","Ruby","C++","C#","PHP",
+      "Swift","Node.js","React","Angular","Vue.js","Bootstrap","Tailwind CSS","jQuery", "Express.js","MongoDB","MySQL"],
+      validate:(value) =>{
+        if(value.length === 0){return "Please Select a language!"} else{return true}
+      },
     },
     {
+        type: 'input',
+        name: 'Installation',
+        message: 'How will you install your project?',
+        validate:(value) => {
+          if (value){return true} else{return "You have to input a Value"}
+        },
+      },
+      {
+        type: 'input',
+        name: 'Usage',
+        message: 'How will your project be used?',
+        validate:(value) => {
+          if (value){return true} else{return "You have to input a Value"}
+        },
+      },
+      {
+        type: 'list',
+        name: 'Licences',
+        message: 'Choice the licences used in the project',
+        choices:["MIT",
+        "Apache 2.0",
+        "The Unlicense",
+        "GNU GPL v3",
+        "BSD 2-Clause",
+        "BSD 3-Clause",
+        "Academic Free License", 
+        "Artistic license",
+        "Boost Software License",
+        "Eclipse Public License",
+        "Eclipse Public License",
+        "European Union Public License",
+        "Microsoft Public License"],
+        validate:(value) => {
+          if (value===0){return "You have to input a value"} else{return true}
+        },
+      },
+    {
       type: 'input',
-      name: 'food',
-      message: 'What is your favorite food?',
+      name: 'Contributors',
+      message: 'Enter Contributor(s) username(s): ',
+      default:'None'
     },
     {
       type: 'input',
       name: 'github',
-      message: 'Enter your GitHub Username',
+      message: 'Enter your Github username',
+      default:'None',
     },
     {
       type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.',
-    }
+      name: 'Contact me',
+      message: 'Enter your email and/or Phone Number ',
+      validate:(value)=>{
+        if(value===''){return 'Contact cannot be blank'}else {return true}
+      },
+    },
 ];
-inquirer.prompt(questions);
-// array of questions for user
-// const generateReadme = (data) => {
-//     inquirer.prompt([{questions}])
-//     .then((response)) => {
-//         fs.write
-//     }
-// }
+// const quest_prompt = inquirer.prompt(questions);
+
 // // function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data)
-        console.log('-----Welcome-----');
+  fs.writeFile(fileName, data);
+  console.log('-----Welcome-----');
 };
 
 
 // function to initialize program
-// function init() {
-
-// }
-
+function init() {
+  console.log('This is a Readme Generator. \nFill in the following info.');
+  inquirer.prompt(questions).then((answers) =>
+  writeToFile('./Professional-Readme/README.md',generateMarkdown({...answers})));
+  console.log('Readme has been created',answers);
+};
+init();
+//console.log(inquirer.prompt(questions));
 // function call to initialize program
 // init();
 //make readme template
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,31 @@
 // function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+function generateMarkdown(answers) {
+  return `
+  # ${answers.Title}
+  ${answers.Description}
+  ## Table Of Contents
+  1.[Languages](#Languages)
+  2.[Installation](#Installation)
+  3.[Usage](#Usage)
+  4.[Licences](#Licences)
+  5.[Contributors](#Contributors)
+  6.[github](#github)
+  7.[Contact-me](#Contact me)
+  
+  
+  ##Languages
+  ${answers.Languages}
+  
+  ##Installation
+  ${answers.Usage}
+  
+  ##Licences
+  ${answers.Licences}
+  
+  ##Contributors
+  ${answers.Contributors}
+  Copyrights (c) 2023 **${answers.Name}**
+  `;
 }
 
 module.exports = generateMarkdown;
